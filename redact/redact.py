c6f3ccbcd1016249e457fad7db3e1ef2c9418931
blavian <benylavian@gmail.com>
2021-11-25 11:19:46 -0500

    hardcoding it works

--- redact/redact.py
+++ redact/redact.py
@@ -1,21 +1,16 @@
 import re
 
 
-filename = "/Users/beny.lavian/Desktop/supervisord.txt"
-output = "/Users/beny.lavian/Desktop/working.txt"
+filename = "/Users/beny.lavian/Desktop/test.txt"
+output = "/Users/beny.lavian/Desktop/test_redacted.txt"
 
 with open(filename, 'r') as infile:
     lines = infile.readlines()
-    pattern = re.compile(r'1\d{10}')
-    print(pattern)
 
 with open(output, 'w') as outfile:
         for line in lines:
-            match = re.match(pattern,line)
-            if match:
-                newline = re.sub(pattern, "redacted", line)
-            else:
-                outfile.write(line)
+                newline = re.sub(r"13233777138", "redacted", line)
+                outfile.write(newline)
 
 
                 
