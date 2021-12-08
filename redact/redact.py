98f3d4c1929504e0d79236f104b95975eca0133d
blavian <benylavian@gmail.com>
2021-12-07 20:30:00 -0500

    formatted

--- redact/redact.py
+++ redact/redact.py
@@ -6,10 +6,7 @@ output = "/Users/beny.lavian/Desktop/test_redacted.txt"
 
 with open(filename, 'r') as infile:
     lines = infile.readlines()
-    pattern= re.compile(
-
-r'''(^|\s|""|'|<)((1|")?\d{10}[^,\.](")?(")?\b|\d{3}-\d{3}-\d{4})|(\(\d+\)\-\d{3}\-\d{4})|(?<=mphone:)1\d{10}|(?<=first_name['"]: )['"].*?['"]|(?<=last_name['"]: )['"].*?['"]|\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b|(?<=['"]address1['"]: )['"](.*?)["'](\s|$|"|\))'''
-)
+    pattern= re.compile(r'''(^|\s|""|'|<)((1|")?\d{10}[^,\.](")?(")?\b|\d{3}-\d{3}-\d{4})|(\(\d+\)\-\d{3}\-\d{4})|(?<=mphone:)1\d{10}|(?<=first_name['"]: )['"].*?['"]|(?<=last_name['"]: )['"].*?['"]|\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b|(?<=['"]address1['"]: )['"](.*?)["'](\s|$|"|\))''')
 # print(pattern)
 
 with open(output, 'w') as outfile:
